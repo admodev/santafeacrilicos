8a92c7c03237d6b62be8700af64a255ce82b506e
admodev <admosdigital@yahoo.com>
2020-10-06 14:59:23 -0300

    agregar conexion con base de datos

---
+++ database/db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `santafeacrilicos`;
+USE `santafeacrilicos`
+
+CREATE TABLE IF NOT EXISTS `admins`;
+INSERT INTO `admins` (id, username, password) VALUES (1, "test", "test123");
---
+++ databaseConnection.php
@@ -0,0 +1,10 @@
+<?php
+
+require('./environmentVariables.php');
+
+$hostname = $_ENV['HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DATABASE_NAME'];
+
+$db = mysqli_connect($hostname, $username, $password, $database);

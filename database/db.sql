328e3fbdc8b8ccda034acbe00298477f6a5587c8
admodev <admosdigital@yahoo.com>
2020-10-15 13:39:49 -0300

    update db.sql syntax

--- database/db.sql
+++ database/db.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE IF NOT EXISTS `santafeacrilicos`;
-USE `santafeacrilicos`
+USE `santafeacrilicos`;
 
-CREATE TABLE IF NOT EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS admins (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     username VARCHAR(255) NOT NULL,
+     password VARCHAR (255) NOT NULL
+    );
 INSERT INTO `admins` (id, username, password) VALUES (1, "test", "test123");
